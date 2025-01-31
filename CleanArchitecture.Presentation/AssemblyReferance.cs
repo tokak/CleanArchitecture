724b4a498d6f9e60381671b5ca32e032d64afac7
Murat Tokak <tokakmurat01@gmail.com>
2025-02-01 01:31:12 +0300

    Presentation Katmanını WebApi Katmanına Bağlama

---
+++ CleanArchitecture.Presentation/AssemblyReferance.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace CleanArchitecture.Presentation
+{
+    public static class AssemblyReferance
+    {
+        public static readonly Assembly  assembly = typeof(Assembly).Assembly;
+    }
+}
--- CleanArchitecture.WepApi/Program.cs
+++ CleanArchitecture.WepApi/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReferance).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
