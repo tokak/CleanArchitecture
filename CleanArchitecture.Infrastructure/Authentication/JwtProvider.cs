07e062c40aa36f25b275c990b126327bd5e7cf06
Murat Tokak <tokakmurat01@gmail.com>
2025-02-14 16:01:21 +0300

    Token oluşturma

---
+++ CleanArchitecture.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchitecture.Application.Abstractions;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Authentication;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string CreateToken(AppUser appUser)
+    {
+        var claims = new Claim[]
+        {
+            new Claim(ClaimTypes.Email,appUser.Email),
+            new Claim(JwtRegisteredClaimNames.Name,appUser.UserName),
+            new Claim("NameLastName",appUser.NameLastName)
+        };
+        JwtSecurityToken jwtSecurityToken = new(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: null,
+            notBefore: DateTime.Now,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),SecurityAlgorithms.HmacSha256));
+
+        string token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        return token;
+
+    }
+}
